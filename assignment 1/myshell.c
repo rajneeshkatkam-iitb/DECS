da0cc32c7db74b38982a4ed39e788007773b3b40
rajneeshkatkam-iitb <69794836+rajneeshkatkam-iitb@users.noreply.github.com>
2020-08-24 02:14:25 +0530

    fork added for ; seperation

--- assignment 1/myshell.c
+++ assignment 1/myshell.c
@@ -13,19 +13,27 @@ int executeCommand(char *str){
     //Check if pipe '|' is present in the command str and split accordingly into two seperate strings to be processed in pipe functionality
     char *str1=NULL, *str2=NULL;
 
-    char *token = strtok(str, "|");
-    str1=token;
-    printf("Token remaining is: %s",str);
-    token=strtok(str,"|");
-    str2=token;
+    str1= strtok(str, "|");
+    
+    if(str1!=NULL)
+        str2=strtok(NULL,"|");
+
 
     if(str2==NULL){
 
         ////No Pipe is present. Perform execution using one child
+
+        //checkIOParser(str1);
+        printf("Without pipe str1 is: %s \n", str1);
+
     }
     else{
         ////Pipe is present. Execute the first command and output of it, feed it to the input of second command
-        
+
+        //checkIOParserPipe(str1, str2);
+                printf("With Pipe str1 is: %s \n", str1);
+                printf("With Pipe str2 is: %s \n", str2);
+
     }
 
     return 1;
@@ -42,6 +50,7 @@ int main(){
         char *input=(char *) malloc(sizeof(char)*max_input_length);
 
         //command prompt print
+        fflush(stdout);
         printf("myShell> ");
         fflush(stdout);
 
@@ -64,34 +73,49 @@ int main(){
 
 
         // Command Seperator function i.e. when it detects ";", then it seperates the command (tokens) and stores them in the commands pointer array 
-        char *commands[10];
-        int command_count=0;
-        char *token = strtok(input, ";");
+        char *command1=NULL, *command2=NULL;
+        command1 = strtok(input, ";");
 
-        while( token != NULL ) {
-            commands[command_count++]=token;
-            token = strtok(NULL, ";");
+
+        if(command1!=NULL)
+            command2 = strtok(NULL, ";");
+
+
+        if(command2==NULL)
+        {
+            
+            printf("command1 is: %s \n", command1);
+            printf("only command 1 executed without fork \n");
+            executeCommand(command1);
         }
-
-        printf("command count: %d \n", command_count);
-
-        ////// Individual Command printer
-        // int print_index=0;
-        // printf("Commands are: \n");
-        // while(print_index<command_index){
-        //     printf("%s \n",commands[print_index++]);
-        // }
-
-        for(int i=0; i<command_count;i++){
-            if(executeCommand(commands[i])==0)
-            {
-                printf("​ Illegal command or arguments");
-                break;
+        else
+        {
+            printf("command1 is: %s \n", command1);
+            printf("command2 is: %s \n", command2);
+            pid_t pid=fork();
+            if(pid==0){
+                
+                printf("command 1 executed with fork \n");
+                executeCommand(command1);
+                exit(0);
             }
+            else{
+                pid_t pid1=fork();
+                if(pid1==0){
+                    printf("command 2 executed with fork \n");
+                    executeCommand(command2);
+                    exit(0);
+                }else{
+                    wait(NULL);
+                    wait(NULL);
+                }
+            }
+
+
         }
 
 
-    }
+    }   
 
     return 0;
 }
